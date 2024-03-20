acfcc3b9b8b160c1d304ee4b5aa6de45c8963279
kazumakano <kazumakano@outlook.jp>
2024-03-20 15:38:54 +0900

    minor update

--- brave.cmd
+++ brave.cmd
@@ -4,11 +4,11 @@ set EXE_FILE="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe
 
 setlocal enabledelayedexpansion
 
-set query=""
+set query=
 for %%a in (%*) do (
     set query=!query!+%%a
 )
 
-%EXE_FILE% "https://www.google.com/search?q=!query:~1!"
+%EXE_FILE% https://www.google.com/search?q=!query!
 
 endlocal
